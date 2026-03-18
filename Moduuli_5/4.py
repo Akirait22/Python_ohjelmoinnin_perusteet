89bd349ad752ab3dd8f99c2aab48673f4ada665c
Akirait22 <akirait@metropolia.fi>
2026-03-18 11:38:04 +0200

    Moduuli 5

---
+++ Moduuli_5/4.py
@@ -0,0 +1,10 @@
+kaupungit = []
+
+for i in range(5):
+    k_nimi = input("Anna kaupungin nimi:" )
+    kaupungit.append(k_nimi)
+
+print("Syötetyt kaupungit: ")
+
+for k_nimi in kaupungit[:1]:
+    print(kaupungit[:5])
