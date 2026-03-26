81d8237f53294ffaa5aa5da201a582bf2e6a2c4c
Akirait22 <akirait@metropolia.fi>
2026-03-26 10:34:28 +0200

    Moduuli 5

--- Kertaustehtävät_26.3/5.py
+++ Kertaustehtävät_26.3/5.py
@@ -5,4 +5,4 @@ luku1= int(input("Anna luku:"))
 luku2= int(input("Anna luku2:"))
 luku3= int(input("Anna luku3:"))
 
-print(f"suurin arvo on {suurin_arvo(luku1, luku2, luku3)}")
+print(f"Suurin arvo on: {suurin_arvo(luku1, luku2, luku3)}")
